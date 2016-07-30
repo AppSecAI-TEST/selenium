3c316d2604afb278d626d34f6d4dcfd8be464f97
ivan-xue <xue.ziheng@gmail.com>
2016-07-30 19:21:50 +1000

    selenium automation framewrok

---
+++ src/utility/Constant.java
@@ -0,0 +1,17 @@
+package utility;
+
+import java.io.File;
+
+/**
+ * Created by ivanxue on 29/07/2016.
+ */
+public class Constant {
+
+    static File rootDir = new File(System.getProperty("user.dir"));
+
+    public static final String Path_TestData = rootDir + "//src//testData//";
+
+    public static final String File_TestData = "TestData.xlsx";
+
+    public static final String URL = "https://unichi.com.au/";
+}
